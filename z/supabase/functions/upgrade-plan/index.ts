915940272c43c30abd35d1f6d359627fcc9388bd
Siraddeen <sircoil17@gmail.com>
2026-07-27 14:47:27 +0530

    finalised version_2

--- z/supabase/functions/upgrade-plan/index.ts
+++
@@ -1,7 +0,0 @@
-// Edge Function: upgrade-plan
-// Purpose: Manually move a user to a higher tier (admin action or post-payment confirmation).
-Deno.serve(async (req: Request) => {
-  return new Response(JSON.stringify({ message: "upgrade-plan: not yet implemented" }), {
-    headers: { "Content-Type": "application/json" },
-  });
-});
